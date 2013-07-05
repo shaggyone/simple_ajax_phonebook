6a21a284965330b15e074d9784763168bfd7b165
Victor Zagorski <victor@zagorski.ru>
2013-07-05 10:58:15 +0800

    Created blank repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestTask2::Application.config.secret_token = '8fc1c442b3092846a14ecffa72805835ced6494cec973adcb9e6f34fecd9bcac75d729b261c448a64171547699a01c9c7bb8a9f566331a61c2b7147b900f5b35'
